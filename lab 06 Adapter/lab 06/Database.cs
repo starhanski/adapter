c77351d06c4bb50ee69a0f986604b3f6378fafc5
Ivan <72632071+starhanski@users.noreply.github.com>
2023-04-11 15:24:00 +0300

    Add files via upload

---
+++ lab 06 Adapter/lab 06/Database.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.Sqlite;
+using Npgsql;
+using System.Data.Common;
+using Microsoft.Data.SqlClient;
+using System.Text.Json;
+using MySql.Data.MySqlClient;
+
+namespace lab_06
+{
+
+    public interface IData
+    {
+        void Connect(string connectionString);
+        void ExecuteQuery(string query);
+        DataTable GetResults();
+    }
+
+    // Адаптер для роботи з базою даних формату MySQL
+    public class MySQLAdapter : IData
+    {
+        private MySqlConnection _connection;
+        private MySqlCommand _command;
+
+        public void Connect(string connectionString)
+        {
+            _connection = new MySqlConnection(connectionString);
+            _command = _connection.CreateCommand();
+            _connection.Open();
+        }
+
+        public void ExecuteQuery(string query)
+        {
+            _command.CommandText = query;
+            _command.ExecuteNonQuery();
+        }
+
+        public DataTable GetResults()
+        {
+            var data = new DataTable();
+            using (var reader = _command.ExecuteReader())
+            {
+                data.Load(reader);
+            }
+            return data;
+        }
+    }
+
+    // Адаптер для роботи з базою даних формату PostgreSQL
+    public class PostgreSQLAdapter : IData
+    {
+        private NpgsqlConnection _connection;
+        private NpgsqlCommand _command;
+
+        public void Connect(string connectionString)
+        {
+            _connection = new NpgsqlConnection(connectionString);
+            _command = _connection.CreateCommand();
+            _connection.Open();
+        }
+
+        public void ExecuteQuery(string query)
+        {
+            _command.CommandText = query;
+            _command.ExecuteNonQuery();
+        }
+
+        public DataTable GetResults()
+        {
+            var data = new DataTable();
+            using (var reader = _command.ExecuteReader())
+            {
+                data.Load(reader);
+            }
+            return data;
+        }
+    }
+
+ 
+    public class Client
+    {
+        private IData _data;
+
+        public Client(IData data)
+        {
+            _data = data;
+        }
+
+        public void ConnectToDatabase(string connectionString)
+        {
+            _data.Connect(connectionString);
+        }
+
+        public void QueryDatabase(string query)
+        {
+            _data.ExecuteQuery(query);
+        }
+
+        public DataTable GetResults()
+        {
+            return _data.GetResults();
+        }
+    }
+
+}
---
+++ lab 06 Adapter/lab 06/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace lab_06
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        void main()
+        {
+            var mysqlAdapter = new MySQLAdapter();
+            var client = new Client(mysqlAdapter);
+            client.ConnectToDatabase("mysql://localhost/mydatabase");
+            client.QueryDatabase("SELECT * FROM mytable");
+            var results = client.GetResults();
+
+            var postgresqlAdapter = new PostgreSQLAdapter();
+            client = new Client(postgresqlAdapter);
+            client.ConnectToDatabase("postgresql://localhost/mydatabase");
+            client.QueryDatabase("SELECT * FROM mytable");
+            results = client.GetResults();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string connectionString = "mysql://localhost/mydatabase";
+            string query = textBox1.Text;
+
+            // підключення до бд
+            IData data;
+            if (connectionString.StartsWith("mysql"))
+            {
+                data = new MySQLAdapter();
+            }
+            else if (connectionString.StartsWith("postgresql"))
+            {
+                data = new PostgreSQLAdapter();
+            }
+            else
+            {
+                throw new Exception("Невідомий формат бази даних.");
+            }
+            var client = new Client(data);
+            client.ConnectToDatabase(connectionString);
+
+            //запит
+            client.QueryDatabase(query);
+            var results = client.GetResults();
+
+            
+            dataGridView1.DataSource = results;
+        }
+    }
+}
